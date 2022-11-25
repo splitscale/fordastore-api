0333bd3cf00b90ebb180ea224f5f9bd3f7874da0
kasutu <jeromecabugwason2@protonmail.com>
2022-11-25 23:21:33 +0800

    init

---
+++ src/main/java/com/splitscale/api/fordastore/FordastoreApplication.java
@@ -0,0 +1,13 @@
+package com.splitscale.api.fordastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FordastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FordastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitscale/api/fordastore/FordastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitscale.api.fordastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FordastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
