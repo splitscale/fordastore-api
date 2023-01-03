7268f96e5bb4d4a929e0fd891faf669f29068016
kasutu <jeromecabugwason2@protonmail.com>
2023-01-03 21:42:41 +0800

    container: Fix return type
    Description:
    fixed  ResponseEntity<List<ContainerResponse>> return type
    
    Expected behavior:
    should not get uid when returning a container list a single container

--- src/main/java/com/splitscale/fordastore/api/container/read/ReadContainerController.java
+++ src/main/java/com/splitscale/fordastore/api/container/read/ReadContainerController.java
@@ -2,6 +2,7 @@ package com.splitscale.fordastore.api.container.read;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.splitscale.fordastore.core.container.Container;
+import com.splitscale.fordastore.core.container.ContainerResponse;
 import com.splitscale.shield.endpoints.container.read.ReadContainerEndpoint;
 
 @RestController
@@ -29,13 +33,22 @@ public class ReadContainerController {
 
   @ResponseBody
   @GetMapping(path = "/{cid}")
-  public ResponseEntity<Container> readContainer(@PathVariable Long containerId,
+  public ResponseEntity<ContainerResponse> readContainer(@PathVariable Long containerId,
       @RequestHeader(value = "authorization") String jwsToken) throws IOException, GeneralSecurityException {
 
-    Container container = endpoint.readByContainerId(containerId, jwsToken);
+    ContainerResponse containerResponse = endpoint.readByContainerId(containerId, jwsToken);
 
-    return new ResponseEntity<>(container, HttpStatus.OK);
+    return new ResponseEntity<>(containerResponse, HttpStatus.OK);
+  }
 
+  @ResponseBody
+  @GetMapping
+  public ResponseEntity<List<ContainerResponse>> readAllContainerByUser(@RequestParam String uid,
+      @RequestHeader(value = "authorization") String jwsToken) throws IOException, GeneralSecurityException {
+
+    List<ContainerResponse> containers = endpoint.readListByUid(uid, jwsToken);
+
+    return new ResponseEntity<List<ContainerResponse>>(containers, HttpStatus.OK);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
