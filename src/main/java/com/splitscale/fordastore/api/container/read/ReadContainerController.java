a10b91671d435b1430456b43cf075f3c3e64c273
kasutu <jeromecabugwason2@protonmail.com>
2023-01-04 16:24:03 +0800

    db: Hotfix URI connection
    Description:
    cant connect to db, need to convert to bridge
    api port should be updated to avoid collision
    updated jws properties
    
    Expected behavior:
    should connect to db

--- src/main/java/com/splitscale/fordastore/api/container/read/ReadContainerController.java
+++ src/main/java/com/splitscale/fordastore/api/container/read/ReadContainerController.java
@@ -10,14 +10,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.splitscale.fordastore.core.container.Container;
 import com.splitscale.fordastore.core.container.ContainerResponse;
 import com.splitscale.shield.endpoints.container.read.ReadContainerEndpoint;
 
