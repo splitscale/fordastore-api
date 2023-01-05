bc1297ec294f314f857a718fd0b2c8d447a781cc
kasutu <jeromecabugwason2@protonmail.com>
2023-01-06 02:59:06 +0800

    cors: Add headers
    Description:
    exposed Authorization header in
    config
    
    Expected behavior:
    should allow Authorization header in response

--- src/main/java/com/splitscale/fordastore/api/auth/AuthController.java
+++ src/main/java/com/splitscale/fordastore/api/auth/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
   }
 
   @ResponseBody
-  @CrossOrigin(exposedHeaders = "Authorization")
+  @CrossOrigin
   @PostMapping(path = "/login")
   public ResponseEntity<UserResponse> login(@RequestBody UserRequest userRequest)
       throws IllegalArgumentException, IOException {
--- src/main/java/com/splitscale/fordastore/api/config/CorsFilter.java
+++ src/main/java/com/splitscale/fordastore/api/config/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter extends OncePerRequestFilter {
     response.addHeader("Access-Control-Allow-Headers",
         "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     response.addHeader("Access-Control-Expose-Headers",
-        "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
     response.addHeader("Access-Control-Allow-Credentials", "true");
     response.addIntHeader("Access-Control-Max-Age", 10);
     filterChain.doFilter(request, response);
