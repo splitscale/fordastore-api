edb5460dc3480d6e95ceb08f5e071f252f2cb9c7
kasutu <jeromecabugwason2@protonmail.com>
2023-01-06 02:13:05 +0800

    added allowed headers

--- src/main/java/com/splitscale/fordastore/api/config/CorsFilter.java
+++ src/main/java/com/splitscale/fordastore/api/config/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter extends OncePerRequestFilter {
     response.addHeader("Access-Control-Allow-Origin", "*");
     response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
     response.addHeader("Access-Control-Allow-Headers",
-        "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     response.addHeader("Access-Control-Expose-Headers",
         "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
     response.addHeader("Access-Control-Allow-Credentials", "true");
